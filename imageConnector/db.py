1d8f4f43f83390281730fcdfe6702db4ea2a6b53
Farrukh Naveed <farrukhnaveed@users.noreply.github.com>
2023-02-20 15:57:05 +0500

    cn: queue loader

--- imageConnector/db.py
+++ imageConnector/db.py
@@ -83,3 +83,98 @@ def updateData(data, imageObj):
     else:
         cnx.close()
 
+def execute(query, type = 'select'):
+    result = []
+    try:
+        cnx = mysql.connector.connect(**mysqlConfig)
+        cursor = cnx.cursor()
+        cursor.execute(query)
+        if type == 'insert':
+            cnx.commit()
+            result = [cursor.lastrowid]
+        else:
+            result = cursor.fetchall()
+        cursor.close()
+        cnx.close()
+    except mysql.connector.Error as err:
+        if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+            print("Something is wrong with your user name or password")
+        elif err.errno == errorcode.ER_BAD_DB_ERROR:
+            print("Database does not exist")
+        else:
+            print(err)
+    else:
+        cnx.close()
+    return result
+
+def get_last_queued_image_id(source):
+    id = None
+    query = "select max(source_id) as source_id from fc_photos_optimization_queue where source = '{}'".format(source)
+    result = execute(query)
+    
+    for (source_id,) in result:
+        if source_id == None:
+            id = 0
+        else:
+            id = source_id
+    return id
+
+
+def add_queue(source, offset, limit):
+    if source == "fc_rental_photos":
+        image = "CONCAT('https://img.cuddlynest.com/images/listings/', product_image_dir, product_image)"
+    else:
+        image = ""
+    
+    query = "SELECT '{}', id, {}, 'added in queue from DAG' FROM {} LIMIT {} OFFSET {}".format(source, image, source, limit, offset)
+    result = execute(query)
+
+    for (table, id, imageUrl, message) in result:
+        query = "INSERT INTO fc_photos_optimization_queue (source, source_id, image, message) VALUES ('{}', {}, '{}', '{}')".format(table, id, imageUrl, message)
+        queue_id = execute(query, 'insert')
+        log(queue_id[0], source, id, 'pending', message)
+
+def log(queue_id, source, source_id, status, message, step = None, error = None, detail = None):
+    insertColumns = []
+    insertValues = []
+    if step == None:
+        step = status
+    if error:
+        insertColumns.append('error')
+        insertValues.append(error)
+    if detail:
+        insertColumns.append('detail')
+        insertValues.append(detail)
+
+    if len(insertColumns) > 0:
+        for i in range(0, len(insertColumns)):
+            if i == 0:
+                insertColumns[i] = "," + insertColumns[i]
+            insertColumns[i] = "'" + insertColumns[i] + "'"
+            insertValues[i] = "'" + insertValues[i] + "'"
+            if i + 1 == len(insertColumns):
+                insertValues[i] = "," + insertValues[i]
+    query = "INSERT INTO fc_photos_optimization_queue_audit_log (queue_id, source, source_id, step, message, status {}) VALUES ({}, '{}', {}, '{}', '{}', '{}' {})".format(",".join(insertColumns), queue_id, source, source_id, step, message, status, ",".join(insertValues))
+    execute(query, 'insert')
+
+
+def getPendingQueueIds(limit):
+    ids = []
+    query = "select * from fc_photos_optimization_queue where status = 'pending' limit %s" % limit
+    result = execute(query)
+    for obj in result:
+        ids.append(obj[0])
+
+    return ids
+
+def updateQueueStatus(ids):
+    status = 'pushed'
+    message = 'pushed to queue'
+    query = "UPDATE fc_photos_optimization_queue set status = '{}', message = '{}' where id in ( {} )".format(status, message, ",".join([str(k) for k in ids]))
+    execute(query, 'insert')
+
+    query = "SELECT id, source, source_id from fc_photos_optimization_queue where id in ( {} )".format(",".join([str(k) for k in ids]))
+    result = execute(query)
+    for (id, source, source_id) in result:
+        log(id, source, source_id, status, message)
+
--- imageConnector/queueLoader.py
+++ imageConnector/queueLoader.py
@@ -1,8 +1,7 @@
 import pika
 import json
-import sys
 import os
-import requests # request img from web
+import db
 from dotenv import load_dotenv
 load_dotenv()
 
@@ -13,9 +12,7 @@ PORT = os.getenv('RABBITMQ_PORT')
 USERNAME = os.getenv('RABBITMQ_USERNAME')
 PASSWORD = os.getenv('RABBITMQ_PASSWORD')
 THRESHOLD = int(os.getenv('RABBITMQ_QUEUE_THRESHOLD'))
-MESSAGE_SIZE = int(os.getenv('RABBITMQ_MESSAGE_SIZE'))
 
-IDS_FILE_PATH = '../lastIdPushed.txt'
 def getQueueCount():
     credentials = pika.PlainCredentials(USERNAME, PASSWORD)
     connection = pika.BlockingConnection(
@@ -29,74 +26,12 @@ def getQueueCount():
     return int(q_len)
 
 
-def getLastPushedId():
-    f = open(IDS_FILE_PATH, "w")
-    f.write(str(0))
-    f.close()
-    if os.path.isfile(IDS_FILE_PATH):
-
-        f = open(IDS_FILE_PATH, "r")
-        id = f.readline()
-        if id == "":
-            return 0
-        else:
-            return int(id)
-    else:
-        return 0
-
-
-def setLastPushedId(newLastId):
-    try:
-        f = open(IDS_FILE_PATH, "w")
-        f.write(str(newLastId))
-        f.close()
-    except Exception as e:
-        raise
-    else:
-        pass
-    finally:
-        pass
-
-def getPropertyImagesParamsDB():
-    lastPushedId = getLastPushedId()
-    newLastId = lastPushedId + THRESHOLD
-    setLastPushedId(newLastId)
-    # imageIds = ",".join([str(k) for k in range(lastPushedId + 1, newLastId + 1)])
-    messages = []
-    tempIds = []
-    count = 0
-    for k in range(lastPushedId + 1, newLastId + 1):
-        tempIds.append(str(k))
-        count = count + 1
-
-        if count >= MESSAGE_SIZE:
-            count = 0
-            messages.append({
-                "source": "db",
-                "table": "BKP_fc_rental_photos",
-                "columns": "product_image_dir,product_image",
-                "imageIds": ",".join(tempIds),
-                "urlPattern": "https://img.cuddlynest.com/images/listings/{}{}",
-            })
-            tempIds = []
-
-    if (len(tempIds) > 0):
-        messages.append({
-            "source": "db",
-            "table": "BKP_fc_rental_photos",
-            "columns": "product_image_dir,product_image",
-            "imageIds": ",".join(tempIds),
-            "urlPattern": "https://img.cuddlynest.com/images/listings/{}{}",
-        })
-    return messages
-
-
 # python callable function
 def push_image_ids():
     queueCount = getQueueCount()
 
     if queueCount < THRESHOLD:
-        messages = getPropertyImagesParamsDB()
+        ids = db.getPendingQueueIds(THRESHOLD)
         credentials = pika.PlainCredentials(USERNAME, PASSWORD)
         connection = pika.BlockingConnection(
             pika.ConnectionParameters(heartbeat=600, blocked_connection_timeout=300, host=HOST, port=PORT, credentials=credentials)
@@ -106,21 +41,30 @@ def push_image_ids():
         channel.queue_declare(queue=QUEUE_NAME, durable=True)
         channel.queue_bind(exchange=EXCHANGE, queue=QUEUE_NAME, routing_key=QUEUE_NAME)
 
-        for message in messages:
+        for id in ids:
             channel.basic_publish(
                 exchange=EXCHANGE,
                 routing_key=QUEUE_NAME,
-                body=json.dumps(message),
+                body=json.dumps({
+                    "queue_id": id
+                }),
                 properties=pika.BasicProperties(
                     delivery_mode=pika.spec.PERSISTENT_DELIVERY_MODE
                 ),
             )
         connection.close()
-        return " [x] Pushed %r" % str(messages)
+        db.updateQueueStatus(ids)
+        return " [x] Pushed %r" % str(ids)
     else:
         return " [x] Not pushed, Queue count: %r" % str(queueCount)
 
 
+def set_queue_product_images():
+    source = "fc_rental_photos"
+    last_image_id = db.get_last_queued_image_id(source)
+    db.add_queue(source, last_image_id, THRESHOLD)
+
 if __name__ == "__main__":
-    result  = push_image_ids()
-    print(result)
+    set_queue_product_images()
+    push_image_ids()
+    # print(result)
